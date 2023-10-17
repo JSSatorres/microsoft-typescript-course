7d6ab0e41817540528bf8c1f9c62984df80bd7cd
jssatorres <juansataz.dev@gmail.com>
2023-10-17 19:34:57 +0200

    feat: object

---
+++ 2-objects.ts
@@ -0,0 +1,36 @@
+// Type Alias
+type Hero = {
+  name: string,
+  age: number
+}
+
+let hero = {
+  name: 'Thor',
+  age: 1500
+}
+
+function createHero(hero: Hero): Hero {
+  const { name, age } = hero
+  return { name, age }
+}
+
+// Optional properties
+type HeroId = `${string}-${string}-${string}-${string}-${string}` // Template union types
+
+type NewHero = {
+  readonly id?: HeroId // commas are optionals (,)
+  name: string
+  age: number
+  isActive?: boolean
+}
+
+function createNewHero(hero: NewHero): NewHero {
+  const { name, age } = hero
+  return {
+      id: crypto.randomUUID(),
+      name,
+      age,
+      isActive: true
+  }
+}
+
