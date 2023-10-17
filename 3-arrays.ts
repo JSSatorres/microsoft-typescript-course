4dadcc3d2d50489370299551f188cf4974bf52a2
jssatorres <juansataz.dev@gmail.com>
2023-10-17 19:38:37 +0200

    feat: array

---
+++ 3-arrays.ts
@@ -0,0 +1,16 @@
+// way 1
+const lenguages1: string[] = []
+lenguages1.push('spanish')
+
+const lenguages2: (string | number)[] = []
+lenguages2.push('spanish')
+lenguages2.push(1)
+
+// way 2
+const lenguages3: Array<string> = []
+lenguages3.push('spanish')
+
+const lenguages4: Array<string | number> = []
+lenguages4.push('spanish')
+lenguages4.push(1)
+
