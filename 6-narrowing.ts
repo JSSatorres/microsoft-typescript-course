283af88568650130970f7088eefc98579ec3d665
jssatorres <juansataz.dev@gmail.com>
2023-10-25 20:04:21 +0200

    feat: narrowing add ohther example

--- 6-narrowing.ts
+++ 6-narrowing.ts
@@ -9,3 +9,26 @@ const displayLength = (object: string | number) => {
 }
 
 displayLength('1')
+
+
+
+// Example 2
+interface Mario {
+  company: string,
+  name: string,
+  jump: () => void
+}
+
+interface Sonic {
+  company: string,
+  name: string,
+  run: () => void
+}
+
+type Character = Mario | Sonic
+
+function play(character: Character) {
+  // console.log(character.run()) -> Error because TS think it could be company or name (mix between both)
+  // Idea 1: Change in Mario interface company: string by company 'Nintendo' and
+  // ask if (character.company === 'Nintendo') { console.log(character.jump()) }
+}
